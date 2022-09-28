cadecfb0de51f7901924ad7ba02539b7d17809d5
Nathan Dyer <69337002+nathanieldyer14@users.noreply.github.com>
2022-09-28 18:37:38 -0500

    dbh.php uploaded

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "gator3410.hostgator.com";
+$username = "boyarone_db";
+$password = "@vs!^9W%pa~+";
+$dbName = $username;
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
